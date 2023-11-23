2d366a65be4f69def976946ca694f0f3930b116f
isabelly <ivrs2@aluno.ifal.edu.br>
2023-11-23 05:14:54 -0300

    acabei página inicial

--- designer.js
+++ designer.js
@@ -1,38 +1,38 @@
 function desenharCards(idBaba, arrayBaba) {
     let containerBaba = document.getElementById(idBaba);
 
-    containerBaba.innerHTML = '';
+    containerBaba.innerHTML = ' ';
 
     for (const Baba of arrayBaba){
         containerBaba.innerHTML +=`
             <div class='card'>
-                <img class='avatar' src='img/${Baba.foto}'>
+            <img class='avatar' src='img/${Baba.foto}'>
                 <div class='conteudo'>
-                    <h3 class='titulo'><a href='detalhes.html?id=${Baba.id}'> ${Baba.nome}</a></h3>
-                    <p class='descricao'> ${Baba.descricao}></p>
+                    <a href='detalhes.html?id=${Baba.id}' class='titulo'><h3 class='titulo'> ${Baba.nome}</a></h3>
+                    <p class='descricao'> ${Baba.descricao}</p>
                 </div>
                 <div class='barra-botoes'>
                     <button class='btn'>
                         <i class = 'fa fa-heart'></i>
                     </button>
                     <button class='btn'>
-                        <i class = 'fa fa-star'></i>
+                        <a href='detalhes.html?id=${Baba.id}'><i class = 'fa fa-plus'></a></i>
                     </button>
                 </div>
-            <div>
+            </div>
         `
     }
 }
 
-function desenharCardDetalhes(idBaba, arrayBaba, idBaba) {
+function desenharCardDetalhes(idBaba, arrayBaba) {
     const Baba = arrayBaba.find((Baba) => Baba.id == idBaba);
     let cardDetalhes = document.getElementById(idBaba);
     cardDetalhes.innerHTML = `
         <img class='avatar' src='img/${Baba.foto}'>
         <div class='conteudo'>
             <h3 class='titulo'><a href='detalhes.html?id=${Baba.id}'> ${Baba.nome}</a></h3>
-            <p class='descricao'> ${Baba.descricao}></p>
-        <div>
+            <p class='descricao'> ${Baba.descricao}</p>
+        </div>
         <div class='botoes'>
             <button class='btn'>
                 <i class = 'fa fa-heart'></i>
--- objeto.js
+++ objeto.js
@@ -1,8 +1,16 @@
-let baba1 = new Baba (20, 'Nazaré Tedesco',  'Cuida da sua criança como se fosse dela', 'foto1.jpg')
-let baba2 = new Baba (201, 'Carminha Tufão', 'Transmite amor com palavras carinhosas', 'foto2.jpeg')
-let baba3 = new Baba (210,'Jezebel', 'Atenção não irá faltar aos seus cuidados. Fique tranquilo!', 'foto3.jpeg' )
-let baba4 = new Baba (211, 'Barbára', 'Acolhimento é sua marca registrada, não irá faltar conforto para sua criança com ela.', 'foto4.jpeg')
+let baba1 = new Baba (20, 'Nazaré Tedesco',  'Cuida da sua criança como se fosse dela.', 'foto1.jpg')
+let baba2 = new Baba (21, 'Carminha Tufão', 'Transmite amor com palavras carinhosas.', 'foto2.jpeg')
+let baba3 = new Baba (22,'Jezebel', 'Atenção não irá faltar aos seus cuidados. Fique tranquilo!', 'foto3.jpeg' )
+let baba4 = new Baba (23, 'Barbára', 'Acolhimento é sua marca registrada, então não irá faltar conforto para sua criança com ela.', 'foto4.jpeg')
+let baba5 = new Baba (24, 'Branca Leticia',  'Trata as adversidades com muita paciência, atenção e experiência.', 'foto5.jpg')
+let baba6 = new Baba (25, 'Marta', 'Sua vontade de acolher todas as crianças torna-se um diferencial.', 'foto6.png')
+let baba7 = new Baba (26,'Flora', 'Guiada por seu coração, transborda diversão.', 'foto7.jpeg' )
+let baba8 = new Baba (27, 'Lia', 'Boas histórias, palavras de carinho e muita empatia não irão faltar.', 'foto8.jpg')
+let baba9 = new Baba (28,'Vitoria', 'Atenciosa nos detalhes, prefere se atentar a forma de cuidado livre.', 'foto9.jpg' )
+let baba10 = new Baba (29, 'Odete Roitman', 'Valoriza a honestidade e passa segurança. Pode ter certeza que sua criança estará bem.', 'foto10.jpg')
+let baba11 = new Baba (28,'Zoé Cruz', 'Seu coração é de mãe e sempre está pronta para cuidar de mais um.', 'foto11.jpg' )
+let baba12 = new Baba (29, 'Eva','É movida pelo contato com a pureza das crianças.', 'foto12.jpg')
 
 let cadastroBaba = []
-cadastroBaba.push(baba1, baba2, baba3, baba4);
+cadastroBaba.push(baba1, baba2, baba3, baba4, baba5, baba6, baba7, baba8, baba9, baba10, baba11, baba12);
 
